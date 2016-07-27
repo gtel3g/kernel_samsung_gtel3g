fd521e64c9d28b4b072d97b01da670b1f5ea6cf6
Quyen <quyenach1234@gmail.com>
2016-07-27 12:59:15 +0700

    zram: Make num_devices equals to total CPU cores
    Preventing performance bottleneck

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -41,7 +41,7 @@ static struct zram *zram_devices;
 static const char *default_compressor = "lzo";
 
 /* Module params (documentation at end) */
-static unsigned int num_devices = 1;
+static unsigned int num_devices = CONFIG_NR_CPUS;
 
 static inline void deprecated_attr_warn(const char *name)
 {
