1f7abb61049ffdb8ff6c9691e8dcd2105f22d1be
Remilia Scarlet <remilia.1505@gmail.com>
2017-09-04 16:45:17 +0700

    mm: Switch to LZ4 compressor for zswap
    Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

--- mm/zswap.c
+++ mm/zswap.c
@@ -81,7 +81,7 @@ static bool zswap_enabled = true;
 module_param_named(enabled, zswap_enabled, bool, 0644);
 
 /* Compressor to be used by zswap (fixed at boot for now) */
-#define ZSWAP_COMPRESSOR_DEFAULT "lzo"
+#define ZSWAP_COMPRESSOR_DEFAULT "lz4"
 static char *zswap_compressor = ZSWAP_COMPRESSOR_DEFAULT;
 module_param_named(compressor, zswap_compressor, charp, 0444);
 
