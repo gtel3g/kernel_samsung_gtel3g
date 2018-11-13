b2f013cc5e186409cf9493429d8f43ba1f8bf686
Ma3aXaTa <valentine@ex.ua>
2018-11-14 00:21:45 +0200

    gtel3g: Import from T561_KK_Opensource

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Wed Jun 3 14:24:01 JST 2015"
+#define LINUX_COMPILE_BY "jung12.shin"
+#define LINUX_COMPILE_HOST "jung-DeskTop"
+#define LINUX_COMPILER "gcc version 4.7 (GCC) "
